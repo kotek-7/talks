33aa947ae51454ca4a9472321d2dbea446536249
kotek <scpktk@gmail.com>
2025-11-05 08:36:50 +0900

    add jiti

--- packages/2025-11-05-functional-programming/uno.config.ts
+++ packages/2025-11-05-functional-programming/uno.config.ts
@@ -1,9 +1,13 @@
-// @ts-expect-error - Ignoring the error of missing types for the uno config
-import config from '@slidev/client/uno.config.ts'
+import { createJiti } from 'jiti'
 import { mergeConfigs, presetWind4 } from 'unocss'
 
+const jiti = createJiti(import.meta.url)
+const baseModule = await jiti.import('@slidev/client/uno.config')
+// @ts-expect-error base config is authored in TypeScript without types
+const baseConfig = baseModule.default ?? baseModule
+
 export default mergeConfigs([
-  config,
+  baseConfig,
   {
     presets: [
       presetWind4(),
