4ceaf4ae518b77d237ba0d9581792eacdcc1066a
kotek <scpktk@gmail.com>
2025-11-05 08:13:54 +0900

    update for node v25

--- packages/2025-11-01-rits-clover-fes/uno.config.ts
+++ packages/2025-11-01-rits-clover-fes/uno.config.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error - Ignoring the error of missing types for the uno config
-import config from '@slidev/client/uno.config'
+import config from '@slidev/client/uno.config.ts'
 import { mergeConfigs, presetWind4 } from 'unocss'
 
 export default mergeConfigs([
